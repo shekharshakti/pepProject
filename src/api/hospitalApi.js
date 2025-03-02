7b4559df5c65645f5e683ad0392cafede521f27a
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2025-03-03 02:43:37 +0530

    Update hospitalApi.js

--- src/api/hospitalApi.js
+++ src/api/hospitalApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'https://pepproject.onrender.com';
 
 // Create a new hospital
 export const createHospital = async (hospitalData) => {
